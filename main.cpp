bbeba0d9106895cc814659ef25499079ff392d11
KonstantinPronin <prosayfer@mail.ru>
2016-10-27 17:24:31 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+#include <curl/curl.h>
+#include <iostream>
+
+int main(){
+        CURL *curl = curl_easy_init();
+        if(curl) {
+                CURLcode res;
+                curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+                res = curl_easy_perform(curl);
+                if(res == CURLE_OK) {
+                        long response_code;
+                        curl_easy_getinfo(curl, CURLINFO_RESPONSE_CODE, &response_code);
+                        std::cout << response_code << std::endl;
+                }
+                curl_easy_cleanup(curl);
+        }
+       return 0;
+}
